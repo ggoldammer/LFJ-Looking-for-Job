127c240df7ff0313c9992435dd3835b4cfc833c1
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2019-01-08 21:18:00 -0700

    Added dependencies and started auth

---
+++ lfj/src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Navbar = () => {
+return (
+        <nav className="navBar-to-be-added">
+            <div className="container-to-be-added">
+                <Link to='/' className="brand-logo">Job Finder NavBar</Link>
+            </div>
+        </nav>
+        )
+}
+
+export default Navbar;
