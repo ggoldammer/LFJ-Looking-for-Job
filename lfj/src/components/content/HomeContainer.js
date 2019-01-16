3548d27967f485eb29b5b297d0db87fdedb130dd
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2019-01-15 20:32:06 -0700

    added Axios api call

--- lfj/src/components/content/HomeContainer.js
+++ lfj/src/components/content/HomeContainer.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { MDBContainer, MDBCol, MDBRow } from "mdbreact";
 import SearchBar from "./SearchBar";
+import ApiResults from "./ApiResults";
+import axios from "axios";
+import request from "request";
 // import "./style.css";
 
 class HomeContainer extends React.Component {
@@ -8,11 +11,39 @@ class HomeContainer extends React.Component {
         return (
             <MDBContainer>
                 <MDBRow>
-                    <SearchBar/>
+                    <SearchBar />
+                    <ApiResults className="" />
                 </MDBRow>
             </MDBContainer>
         );
     }
 }
 
+axios({
+    method: 'get',
+    url: "https://data.usajobs.gov/api/search?Keyword=engineer",
+    headers: {
+        "Host": process.env.REACT_APP_HOST,
+        "User-Agent": process.env.REACT_USER_AGENT,
+        "Authorization-Key": process.env.REACT_APP_USA_KEY
+    }
+})
+    .then(function (response) {
+        console.log(response);
+    });
+
+// request({
+//     url: 'https://data.usajobs.gov/api/search',
+//     method: 'GET',
+//     headers: {
+//         "Host": process.env.REACT_APP_HOST,
+//         "User-Agent": process.env.REACT_USER_AGENT,
+//         "Authorization-Key": process.env.REACT_APP_USA_KEY
+//     }
+// }, function (error, response, body) {
+//     console.log(response);
+// });
+
+
 export default HomeContainer;
+
