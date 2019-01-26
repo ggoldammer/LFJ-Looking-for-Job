6db3682914a9901e313ac8716cb0b1b896466ff2
Gabriel Viveros-Goldammer <gabrielgoldammer@gmail.com>
2019-01-26 12:36:52 -0700

    Made style changes

--- src/components/content/MainContent/AboutUs/AboutUs.js
+++ src/components/content/MainContent/AboutUs/AboutUs.js
@@ -1,22 +1,66 @@
 import React from 'react';
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 
 const AboutUs = () => {
-  return (
-    <MDBCol>
-      <MDBCard style={{ width: "22rem" }}>
-        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            Some quick example text to build on the card title and make
-            up the bulk of the card&apos;s content.
-          </MDBCardText>
-          <MDBBtn href="#">MDBBtn</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
-  )
+    return (
+        <MDBContainer>
+            <MDBRow>
+                <MDBCol>
+                    <MDBCard style={{ width: "17rem" }}>
+                        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+                        <MDBCardBody>
+                            <MDBCardTitle>Andres Gonzalez</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make
+                                up the bulk of the card&apos;s content.
+                            </MDBCardText>
+                            <MDBBtn href="#">MDBBtn</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard style={{ width: "17rem" }}>
+                        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+                        <MDBCardBody>
+                            <MDBCardTitle>Fernando Aguilar</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make
+                                up the bulk of the card&apos;s content.
+                            </MDBCardText>
+                            <MDBBtn href="#">MDBBtn</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard style={{ width: "17rem" }}>
+                        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+                        <MDBCardBody>
+                            <MDBCardTitle>Gabriel Viveros-Goldammer</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make
+                                up the bulk of the card&apos;s content.
+                            </MDBCardText>
+                            <MDBBtn href="#">MDBBtn</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+                <MDBCol>
+                    <MDBCard style={{ width: "17rem" }}>
+                        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+                        <MDBCardBody>
+                            <MDBCardTitle>Sergio Trujillo</MDBCardTitle>
+                            <MDBCardText>
+                                Some quick example text to build on the card title and make
+                                up the bulk of the card&apos;s content.
+                            </MDBCardText>
+                            <MDBBtn href="#">MDBBtn</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+
+    )
 }
 
 export default AboutUs;
