587cc9590fe8e06cf07ffd5e90696c918259b049
unknown <gaboguy@gmail.com>
2019-01-27 16:07:16 -0700

    Adjusted entire Navbar structure for auth

--- src/components/content/Navbar/NavbarSection.js
+++ src/components/content/Navbar/NavbarSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, FormInline, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Fa, MDBBtn } from "mdbreact";
 import fire from "../../config/Fire";
+import SignedOutLinks from "./SignedOutLinks"
 
 class NavbarSection extends React.Component {
   constructor(props){
@@ -29,39 +30,14 @@ class NavbarSection extends React.Component {
     })
   }
 
-  toggleCollapse = collapseID => () =>
-    this.setState(prevState => ({
-      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-    }));
+  // toggleCollapse = collapseID => () =>
+  //   this.setState(prevState => ({
+  //     collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+  //   }));
 
   render() {
     return (
-      <Navbar color="default-color" dark expand="md">
-        <NavbarBrand>
-          <strong className="white-text">LFJ App</strong>
-        </NavbarBrand>
-        <NavbarToggler
-          onClick={this.toggleCollapse("navbarCollapse")}
-        />
-        <Collapse id="navbarCollapse" isOpen={this.state.collapseID} navbar>
-          <NavbarNav left>
-            <NavItem active>
-              <NavLink to="/">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to="/about">About</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink to="/signin">SignIn</NavLink>
-            </NavItem>
-          </NavbarNav>
-          <NavbarNav right>
-            <NavItem>
-              <MDBBtn to="/signin" color="primary">Login</MDBBtn>
-            </NavItem>
-          </NavbarNav>
-        </Collapse>
-      </Navbar>
+      <SignedOutLinks />
     );
   }
 }
--- src/components/content/Navbar/SignedOutLinks.js
+++ src/components/content/Navbar/SignedOutLinks.js
@@ -1,16 +1,48 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavbarNav, NavItem, NavLink, MDBCollapse, MDBBtn, NavbarToggler, Navbar, NavbarBrand } from "mdbreact";
 
 
-const SignedOutLinks = () => {
-return (
-    <ul className="nav">
-        <li className="nav-item"><NavLink to='/'>Home</NavLink></li>
-        <li className="nav-item"><NavLink to='/'>About</NavLink></li>
-        <li className="nav-item"><NavLink to='/'>Sign Up</NavLink></li>
-        <li className="nav-item"><NavLink to='/'>Log In</NavLink></li>
-    </ul>
+class SignedOutLinks extends React.Component {
+    state = {
+        collapseID: ""
+    }
+
+    toggleCollapse = collapseID => () =>
+        this.setState(prevState => ({
+            collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+        }));
+
+    render() {
+        return (
+            <Navbar color="default-color" dark expand="md">
+                <NavbarBrand>
+                    <strong className="white-text">LFJ App</strong>
+                </NavbarBrand>
+                <NavbarToggler
+                    onClick={this.toggleCollapse("navbarCollapse")}
+                />
+                <MDBCollapse id="navbarCollapse" isOpen={this.state.collapseID} navbar>
+                    <NavbarNav left>
+                        <NavItem active>
+                            <NavLink to="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/signin">SignIn</NavLink>
+                        </NavItem>
+                    </NavbarNav>
+                    <NavbarNav right>
+                        <NavItem>
+                            <MDBBtn to="/signin" color="primary">Login</MDBBtn>
+                        </NavItem>
+                    </NavbarNav>
+                </MDBCollapse>
+            </Navbar>
+
         )
+    }
 }
 
 export default SignedOutLinks;
